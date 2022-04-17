aea061049f2df3955d34578899c64075e3ea75d3
seyfe-geb <syberglife@gmail.com>
2022-04-16 20:33:48 -0500

    Initial commit

---
+++ src/main/java/net/groupseven/onlinemarketg7backend/OnlineMarketG7BackendApplication.java
@@ -0,0 +1,13 @@
+package net.groupseven.onlinemarketg7backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMarketG7BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMarketG7BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/groupseven/onlinemarketg7backend/OnlineMarketG7BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.groupseven.onlinemarketg7backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMarketG7BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
